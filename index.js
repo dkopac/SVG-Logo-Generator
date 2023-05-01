144e6c6c04dc3560021ddca81e8311586c34ed36
[Dan <123705636+dkopac@users.noreply.github.com>
2023-05-01 10:13:33 -0400

    updated Generated logo.svg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const questions = require("./lib/questions.js");
-const fileName = "./logo.svg";
+const fileName = "./Generated logo.svg";
 const setShape = require("./lib/setShape.js");
 
 // function to create new svg file using inquirer response and file system
